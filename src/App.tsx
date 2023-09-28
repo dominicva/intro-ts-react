c1bb721f83a1b306071ed56624d9ffc8014169fa
Dominic van Almsick <dominicvana@gmail.com>
2023-09-28 14:27:10 +0100

    refactor: remove unnecessary div

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,23 @@ import countries from './countries.json'
 function CountryCard({ name, continents, population, capital, flags }) {
   return (
     <article className="country">
-      <div>
-        <img src={flags.svg} alt={flags.alt} className="country__flag" />
-        <div className="country__text-content">
-          <hgroup>
-            <h2 className="country__name">{name}</h2>
-            <h3 className="country__continent">{continents[0]}</h3>
-          </hgroup>
-          <div className="country__info">
-            <p className="country__info-container">
-              <span className="country__label">Capital city</span>
-              <span className="country__value">{capital}</span>
-            </p>
-            <p className="country__info-container">
-              <span className="country__label">Population</span>
-              <span className="country__value">
-                {population.toLocaleString()}
-              </span>
-            </p>
-          </div>
+      <img src={flags.svg} alt={flags.alt} className="country__flag" />
+      <div className="country__text-content">
+        <hgroup>
+          <h2 className="country__name">{name}</h2>
+          <h3 className="country__continent">{continents[0]}</h3>
+        </hgroup>
+        <div className="country__info">
+          <p className="country__info-container">
+            <span className="country__label">Capital city</span>
+            <span className="country__value">{capital}</span>
+          </p>
+          <p className="country__info-container">
+            <span className="country__label">Population</span>
+            <span className="country__value">
+              {population.toLocaleString()}
+            </span>
+          </p>
         </div>
       </div>
     </article>
